d82beaa1d48bda0fe98e4a8a868351649aea562e
Naz Islam <naz@nazislam.com>
2018-10-01 01:32:37 -0400

    Copied code from branch - angularSrc

---
+++ client/src/app/models/user.model.ts
@@ -0,0 +1,14 @@
+export class User {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public gender: string,
+    public major: string,
+    public university: string,
+    // public DateOfBirth: string,
+    public email: string,
+    public password: string
+  ) {
+    
+  }
+}
